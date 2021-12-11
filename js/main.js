b12f89467d165c13ad61665427116eb6e8513ec8
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2021-12-11 20:44:22 +0700

    add discover

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@ window.addEventListener("scroll", function () {
     var nav = this.document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// Swiper discover
+var swiper = new Swiper(".discover-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 32,
+    coverflowEffect: {
+        rotate: 0,
+    },
+});
